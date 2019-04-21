df89fcc20d7144e11b6a73c6ae981ee3b98f46ad
poby <834971685@qq.com>
2019-04-21 18:57:03 +0800

    完成模板引擎

--- View/Compiled/AexCompiler.php
+++ View/Compiled/AexCompiler.php
@@ -8,12 +8,39 @@
 
 namespace View\Compiled;
 
+use View\Compiled\Concerns\CompilesEchos;
 use View\Support\Filesystem;
 
 class AexCompiler extends Compiler implements CompilerInterface
 {
+    use CompilesEchos;
 
     protected $path;
+
+    protected $contentTags = ['{{', '}}'];
+
+    protected $escapedTags = ['{{{', '}}}'];
+
+    protected $rawTags = ['{!!', '!!}'];
+
+    protected $echoFormat = 'e(%s)';
+
+    protected $compilers = [
+        'Echos',
+    ];
+
+    public function compile($path)
+    {
+        if ($path) {
+            $this->setPath($path);
+        }
+
+        $result = $this->compileStatements(($this->files->get($path)));
+
+        $result = $this->compileContents($result);
+
+        $this->files->put($this->getCompiledPath($this->path), $result);
+    }
 
     protected function compileStatements($value)
     {
@@ -29,19 +56,16 @@ class AexCompiler extends Compiler implements CompilerInterface
         return isset($match[3]) ? $match[1] . $match[3] : $match[1];
     }
 
-    public function compile($path)
-    {
-        if ($path) {
-            $this->setPath($path);
-        }
-
-        $result = $this->compileStatements(($this->files->get($path)));
-
-        $this->files->put($this->getCompiledPath($this->path), $result);
-    }
-
     public function setPath($path)
     {
         $this->path = $path;
+    }
+
+    protected function compileContents($content)
+    {
+        foreach ($this->compilers as $type) {
+            $content = $this->{"compile{$type}"}($content);
+        }
+        return $content;
     }
 }
