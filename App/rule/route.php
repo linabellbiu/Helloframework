17d98c20cdc5f31f1f7af60b3f1cedaadbeabed3
poby <834971685@qq.com>
2019-05-04 17:23:40 +0800

    测压

--- App/rule/route.php
+++ App/rule/route.php
@@ -3,12 +3,12 @@
  * nullable 可选字段，可以不填，填了必须验证
  */
 
-\Core\RouteService::get('IndexController@index', '/', [
-    'm' => 'mail|nullable',
-])->rulesCookie([
-    'language' => 'mail'
-])->bandingError([
-    'm|string' => ['zh' => '中文错误', 'en' => 'english error'],
-    'm|mail' => ['zh' => '不是正确的邮箱', 'en' => 'this is not e-mail'],
-    'language|mail' => ['zh' => '不是正确的邮箱', 'en' => 'this is not e-mail'],
-]);
+//\Core\RouteService::get('IndexController@index', '/', [
+//    'm' => 'mail|nullable',
+//])->rulesCookie([
+//    'language' => 'mail'
+//])->bandingError([
+//    'm|string' => ['zh' => '中文错误', 'en' => 'english error'],
+//    'm|mail' => ['zh' => '不是正确的邮箱', 'en' => 'this is not e-mail'],
+//    'language|mail' => ['zh' => '不是正确的邮箱', 'en' => 'this is not e-mail'],
+//]);
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Helloframework
 ### PHPmvc框架
+
+```
+一千个请求每次100并发输出hello word模板,windows\4核cpu
+Percentage of the requests served within a certain time (ms)
+    50%    317
+    66%    332
+    75%    348 
+    80%    359
+    90%    387
+    95%    410
+    98%    480
+    99%    496
+    100%    507 (longest request)
+最大的响应时间小于 507 毫秒
+
+Concurrency Level:      100
+Time taken for tests:   3.275 seconds
+Complete requests:      1000
+Failed requests:        0
+Requests per second:    305.36 [#/sec] (mean)
+Time per request:       327.481 [ms] (mean)
+Time per request:       3.275 [ms] (mean, across all concurrent requests)
+Transfer rate:          527.52 [Kbytes/sec] received
+```
